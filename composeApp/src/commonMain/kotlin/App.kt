996bdb6f2d0a6b441a69e641d24bb360c88df7fb
jpmuedano <113537046+jpmuedano@users.noreply.github.com>
2024-08-04 21:30:36 -0400

    Design Format

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,13 +1,17 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import utils.currentTimeAt
 
@@ -18,13 +22,24 @@ fun App() {
         var location by remember { mutableStateOf("Europe/Paris") }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
 
-        Column {
-            Text(timeAtLocation)
-            TextField(value = location, onValueChange = { location = it })
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Text(
+                text = timeAtLocation,
+                style = TextStyle(fontSize = 20.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
+            )
+            TextField(
+                modifier = Modifier.padding(top = 10.dp),
+                value = location,
+                onValueChange = { location = it })
             Button(
+                modifier = Modifier.padding(top = 10.dp),
                 onClick = { timeAtLocation = currentTimeAt(location) ?: "Invalid location" }
             ) {
-                Text("Show Time at Location")
+                Text("Show Time")
             }
         }
     }
